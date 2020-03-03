51eb70af85eeb8ec15c5696cf02617a134be71c9
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-03-03 11:40:40 -0700

    Add project files.

---
+++ PlayingWithGenerics/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayingWithGenerics
+{
+    public class Employee<T>
+    {
+        public Employee()
+        {
+            Things = new List<T>();
+        }
+
+        public List<T> Things { get; set; }
+
+        // Shows the list nicely.
+        public void ShowList()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < Things.Count; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sb.Append("\n");
+                }
+
+                sb.Append(String.Format("{0,25}  ", Things[i].ToString()));
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ PlayingWithGenerics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlayingWithGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Some random things that an employee might have.
+            List<string> things = new List<string>()
+                { "Cool Thing", "Ball of Yarn", "Collection of Mirrors",
+                  "Magestic Table Cloth", "An Apple", "Super Cool Pencil"};
+
+            List<int> numbers = new List<int>()
+                { 1, 3, 5, 6, 2, 2, 2, 1, 5, 6, 1, 5};
+
+
+            Employee<string> employee1 = new Employee<string>();
+
+            Console.WriteLine("Check out this Employee with his things!");
+            System.Threading.Thread.Sleep(1000);
+            employee1.Things = things;
+            employee1.ShowList();
+
+            System.Threading.Thread.Sleep(2000);
+
+            Employee<int> employee2 = new Employee<int>();
+
+            Console.WriteLine("\nCheck out this Employee with his numbers!");
+            System.Threading.Thread.Sleep(1000);
+            employee2.Things = numbers;
+            employee2.ShowList();
+
+            Console.ReadLine();
+        }
+    }
+}
